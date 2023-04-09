b94e791e15e5ee39dd705be9df2bd700d2d04c90
PedroGarganta <pedrogarganta00@gmail.com>
2023-04-09 20:10:59 -0300

    Barra de navegacion responsive

--- app.js
+++ app.js
@@ -121,37 +121,15 @@ window.onscroll = function(){
 }
 
 //función que muestra el menu responsive{
-function responsiveMenu(){
-    let x = document.getElementById("nav");
-    if(x.className===""){
-        x.className = "responsive";
+let abrirmenu = document.querySelector('.menu-responsive');
+let abrirmenuicono = document.querySelector('.menu-responsive i');
+let menu = document.getElementById('menu');
 
-        //creamos el elemento que cierra el menu
-        let span = document.createElement("span");
-        span.innerHTML = "X";
-        document.getElementById("nav").appendChild(span);
-
-        //quitamos el boton eliminar cuando se hace click sobre este
-        span.onclick = function(){
-            x.className = "";
-            span.remove();
-        }
+abrirmenu.addEventListener('click', e=>{
+    menu.classList.toggle('mostrar');
+    if(menu.classList.contains('mostrar')){
+        abrirmenuicono.setAttribute('class', "fa fa-times")
     }else{
-        x.className="";
+        abrirmenuicono.setAttribute("class", "fa fa-bars")
     }
-}
-
-//Este codigo es para agregar una función a cada links del menu
-//responsive
-//cuando se presione en cualquier de los links del menu debe desaparecer el menu
-const links = document.querySelectorAll("#nav a");
-for(i=0; i < links.length;i++){
-    links[i].onclick = function(){
-        var x = document.getElementById("nav");
-        x.className = "";
-
-        //removemos el boton eliminar
-        btnEliminar = document.querySelector("#nav span");
-        btnEliminar.remove();
-    }
-}
+});  
